f42d08198844e6e9b833caab80a33e24dfed7474
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-14 16:48:18 +0800

    AM  01st commit

---
+++ src/main/java/com/atguigu/basic01/_01_SwapBoth.java
@@ -0,0 +1,78 @@
+package com.atguigu.basic01;
+
+import org.junit.Test;
+
+/**
+ * Description==>TODO
+ * BelongsProject==>algorithm_basic01
+ * BelongsPackage==>com.atguigu.basic01
+ * CreateTime==>2022-09-12 07:21:43
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+public class _01_SwapBoth {
+
+    @Test
+    public void test03() {
+
+        int num01 = 10;
+
+        int num02 = 20;
+
+//        System.out.println("交换之前 >> " + num01 + "," + num02);
+
+        num01 = num01 + num02;
+
+        num02 = num01 - num02;
+
+        num01 = num01 - num02;
+
+//        System.out.println("交换之后 >> " + num01 + "," + num02);
+
+
+    }
+
+    @Test
+    public void test02() {
+
+        int num01 = 10;
+
+        int num02 = 20;
+
+//        System.out.println("交换之前 >> " + num01 + "," + num02);
+
+        num01 ^= num02;
+
+        num02 ^= num01;
+
+        num01 ^= num02;
+
+//        System.out.println("交换之后 >> " + num01 + "," + num02);
+
+    }
+
+
+    @Test
+    public void test01() {
+
+        int num01 = 10;
+
+        int num02 = 20;
+
+        int num03;
+
+//        System.out.println("交换之前 >> "+num01+","+num02);
+
+        num03 = num01;
+
+        num01 = num02;
+
+        num02 = num03;
+
+//        System.out.println("交换之后 >> "+num01+","+num02);
+
+
+    }
+
+
+}
